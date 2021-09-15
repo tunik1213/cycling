f7ec529b388ace95e3387abef5129f981b3d9b09
tunik <tunik@detali.zp.ua>
2021-09-15 13:55:52 +0300

    ошибка 404

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,5 @@
+@extends('layout')
+@section('content')
+	<h1>Сторiнку не знайдено</h1>
+	<a href="/">← На головну</a>
+@endsection
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -16,7 +16,7 @@
     
     @else
 
-
+        
 
     @endguest
     </div>
