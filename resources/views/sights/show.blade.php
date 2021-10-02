152b2ec6e16eb95f217ddb295cc6a60b4f89c39e
tunik <tunik@detali.zp.ua>
2021-10-02 06:52:53 +0300

    божеский вид страницы допримечательности

--- resources/views/sights/show.blade.php
+++ resources/views/sights/show.blade.php
@@ -1,49 +1,43 @@
 @extends('layout')
 @section('content')
 
-@if($sight->district)
-<nav aria-label="breadcrumb">
-  <ol class="breadcrumb">
-    <li class="breadcrumb-item"><a href="{{ route('areas.show',$sight->district->area->id) }}">{{ $sight->district->area->name }}</a></li>
-    <li class="breadcrumb-item"><a href="{{ route('districts.show',$sight->district->id) }}">{{ $sight->district->name }}</a></li>
-    <li class="breadcrumb-item active" aria-current="page">{{ $sight->name }}</li>
-  </ol>
-</nav>
-@endif
+<div class="row">
+    <div class="col-sm-4 col-xs-12">
+        <img class="sight-image" src="data:image/jpeg;base64,{{base64_encode($sight->image)}}" alt="Фото {{$sight->name}}">
+    </div>
 
-    <div class="row">
-        <div class="col-lg-12 margin-tb">
-            <div class="pull-left">
-                <h2>{{ $sight->name }}</h2>
-            </div>
-{{--             <div class="pull-right">
-                <a class="btn btn-primary" href="{{ route('sights.index') }}">← Назад</a>
-            </div> --}}
+    <div class="col-xl-4 col-sm-8 col-xs-12">
+        <h2>{{ $sight->name }}</h2>
+
+        @if($sight->district)
+            <nav aria-label="breadcrumb">
+                <ol class="breadcrumb">
+                    <li class="breadcrumb-item"><a href="{{ route('areas.show',$sight->district->area->id) }}">{{ $sight->district->area->name }} область</a></li>
+                    <li class="breadcrumb-item"><a href="{{ route('districts.show',$sight->district->id) }}">{{ $sight->district->name }} район</a></li>
+                    {{-- <li class="breadcrumb-item active" aria-current="page">{{ $sight->name }}</li> --}}
+                </ol>
+            </nav>
+        @endif
+
+        <div id="sight-author">
+            <strong>Автор:</strong>
+            @if($sight->user)
+                <a href="{{route('userProfile',$sight->user->id)}}">{{ $sight->user->firstname }}</a>
+            @else
+                Google
+            @endif
         </div>
     </div>
 
 
-    @if($sight->user)
-        <div class="col-xs-12 col-sm-12 col-md-12">
-            <div class="form-group">
-                <strong>Автор:</strong>
-                <a href="{{route('userProfile',$sight->user->id)}}">{{ $sight->user->firstname }}</a>
-            </div>
-        </div>   
-    @endif
 
-    <div id="sight-images-container">
-        <div>
-            <img class="float-left" src="data:image/jpeg;base64,{{base64_encode($sight->image)}}" alt="Фото {{$sight->name}}">
-        </div>
-        <div>
-            <a href="{{$sight->gm_link()}}" target="_blank" rel="nofollow">
-                <img class="float-right" src="data:image/jpeg;base64,{{base64_encode($sight->map_image)}}" alt="Мапа {{$sight->name}}"> 
-            </a>
-        </div>
-    </div>  
-        
+    <div class="col-xl-4 col-md-12">
+        <a href="{{$sight->gm_link()}}" target="_blank" rel="nofollow">
+            <img class="sight-image" src="data:image/jpeg;base64,{{base64_encode($sight->map_image)}}" alt="Мапа {{$sight->name}}"> 
+        </a>
+    </div>
 
+</div>
         
         
 
