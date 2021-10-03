1f98921d435be4bb33d1e9a9d88b6a3c025c8715
tunik <tunik@detali.zp.ua>
2021-10-03 07:39:02 +0300

    цвет подложки

--- resources/views/production_asserts.blade.php
+++ resources/views/production_asserts.blade.php
@@ -4,7 +4,7 @@
 @endphp
 
 @if($type=='css')
-    <link href="{{ asset('build/20211003014236.css') }}{{$gz}}" rel="stylesheet">
+    <link href="{{ asset('build/20211003073838.css') }}{{$gz}}" rel="stylesheet">
 @elseif($type=='js')
-    <script src="{{ asset('build/20211003014236.js') }}{{$gz}}"></script>
+    <script src="{{ asset('build/20211003073838.js') }}{{$gz}}"></script>
 @endif
