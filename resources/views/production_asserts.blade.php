68e82e5b11e7cfb9324750ef09a638b590f59f1b
tunik <tunik@detali.zp.ua>
2021-10-18 07:45:24 +0300

    уменьшено пространство перед подвалом

--- resources/views/production_asserts.blade.php
+++ resources/views/production_asserts.blade.php
@@ -4,7 +4,7 @@
 @endphp
 
 @if($type=='css')
-    <link href="{{ asset('build/20211018000408.css') }}{{$gz}}" rel="stylesheet">
+    <link href="{{ asset('build/20211018074507.css') }}{{$gz}}" rel="stylesheet">
 @elseif($type=='js')
-    <script src="{{ asset('build/20211018000408.js') }}{{$gz}}"></script>
+    <script src="{{ asset('build/20211018074507.js') }}{{$gz}}"></script>
 @endif
