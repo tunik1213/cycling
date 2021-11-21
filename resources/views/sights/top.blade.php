2388a30dff328fa84f1258777b63226dd91f5210
tunik <tunik@detali.zp.ua>
2021-11-21 17:18:18 +0200

    fix

--- resources/views/sights/top.blade.php
+++ resources/views/sights/top.blade.php
@@ -10,38 +10,38 @@
 	$title = 'Топ '.$wich.'пам\'яток'	
 @endphp
 
-<div id="top-sights-visited">
+<div class="top-sights">
 	<div class="container info-block">
-	<h2 class="info-block-header">{{$title}}</h2>
-	<div class="info-block-body">
-		@include('sights.list_partial',['sightList'=>$topSights])
-	</div>
-	<div class="info-block-footer">
-		@php($show_total_link = true)
+		<h2 class="info-block-header">{{$title}}</h2>
+		<div class="info-block-body">
+			@include('sights.list_partial',['sightList'=>$topSights])
+		</div>
+		<div class="info-block-footer">
+			@php($show_total_link = true)
 
- 		@if(!empty($user))
+	 		@if(!empty($user))
 
-			@if($user->activities->count() == 0)
-				@php($show_total_link = false)
-				<p>Не вдалося iмпортувати данi @if($itsMe) по Вашим заїздам@endif зi Strava</p>
-			@else
-				@if($loading)
+				@if($user->activities->count() == 0)
 					@php($show_total_link = false)
-					<span class="spinner-border spinner-border-sm" role="status"></span>
-					&nbsp;&nbsp;&nbsp;&nbsp;
-					<span>Наразi проводиться аналiз Ваших заїздiв</span>
-					<script>
-						setTimeout(function(){
-						   window.location.reload(1);
-						}, 20000);
-					</script>
+					<p>Не вдалося iмпортувати данi @if($itsMe) по Вашим заїздам@endif зi Strava</p>
+				@else
+					@if($loading)
+						@php($show_total_link = false)
+						<span class="spinner-border spinner-border-sm" role="status"></span>
+						&nbsp;&nbsp;&nbsp;&nbsp;
+						<span>Наразi проводиться аналiз Ваших заїздiв</span>
+						<script>
+							setTimeout(function(){
+							   window.location.reload(1);
+							}, 20000);
+						</script>
+					@endif
 				@endif
 			@endif
-		@endif
 
-		@if($show_total_link)
-			<a class="link-secondary" href="{{route('sights.list',$topSights->filters())}}">Переглянути всi</a>
-		@endif
-
+			@if($show_total_link)
+				<a class="link-secondary" href="{{route('sights.list',$topSights->filters())}}">Переглянути всi</a>
+			@endif
+		</div>
 	</div>
 </div>
