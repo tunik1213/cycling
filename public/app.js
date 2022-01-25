7535d5af2f5b12dae8a648f00a0a5cadb7bc8a14
tunik <tunik@detali.zp.ua>
2022-01-25 11:04:20 +0200

    оставлять раскрытой категорию, по которой осуществлен отбор в списке

--- public/app.js
+++ public/app.js
@@ -34,13 +34,18 @@ var listTree = function() {
 		e.preventDefault();
 	});
 
-	var toggler = document.getElementsByClassName("caret");
-	var i;
+	$('i.caret').click(function(e){
+		$(this).closest('li').toggleClass('active');
+	});
 
-	for (i = 0; i < toggler.length; i++) {
-	  toggler[i].addEventListener("click", function() {
-	    this.parentElement.querySelector(".nested").classList.toggle("active");
-	    this.classList.toggle("caret-down");
-	  });
-	}
+
+	// var toggler = document.getElementsByClassName("caret");
+	// var i;
+
+	// for (i = 0; i < toggler.length; i++) {
+	//   toggler[i].addEventListener("click", function() {
+	//     this.parentElement.querySelector(".nested").classList.toggle("active");
+	//     this.classList.toggle("caret-down");
+	//   });
+	// }
 }
--- resources/views/sights/list.blade.php
+++ resources/views/sights/list.blade.php
@@ -1,3 +1,11 @@
+@php
+    $filter_area_id = $sightList->area->id ?? $sightList->district->area->id ?? null;
+    $filter_district_id = $sightList->district->id ?? null;
+    $filter_category_id = $sightList->category->id ?? $sightList->subcategory->category->id ?? null;
+    $filter_subcategory_id = $sightList->subcategory->id ?? null;
+@endphp
+
+
 @extends('layout')
 
 @section('head')
@@ -34,13 +42,16 @@
                         @php($getParams = $sightList->filters([],['area','district']))
                         <li><a href="{{route('sights.list',$getParams)}}">Вся Україна</a></li>
                     @foreach($sightList->locations() as $id=>$area)
-                        <li><i class="fas fa-chevron-right caret"></i>
-                            @php($getParams = $sightList->filters(['area'=>$id],['district']))
-                            <a href="{{route('sights.list',$getParams)}}">{{$area['name']}}</a>
+                        @php($getParams = $sightList->filters(['area'=>$id],['district']))
+                        @php($active=($id == $filter_area_id) ? 'active' : '')
+                        @php($bold = ($active) ? 'bold' : '')
+                        <li class="{{$active}}"><i class="fas fa-chevron-down caret"></i>
+                            <a class="{{$bold}}" area-id="{{$id}}" href="{{route('sights.list',$getParams)}}">{{$area['name']}}</a>
                             <ul class="nested folding">
                             @foreach($area['districts'] as $d_id=>$d_name)
                                 @php($getParams = $sightList->filters(['district'=>$d_id],['area']))
-                                <li><a class="link-secondary" href="{{route('sights.list',$getParams)}}">{{$d_name}}</a></li>
+                                @php($bold = ($filter_district_id == $d_id) ? 'bold' : '')
+                                <li><a district-id="{{$d_id}}" class="link-secondary {{$bold}}" href="{{route('sights.list',$getParams)}}">{{$d_name}}</a></li>
                             @endforeach
                             </ul>
                         </li>
@@ -52,13 +63,16 @@
                         @php($getParams = $sightList->filters([],['category','subcategory']))
                         <li><a href="{{route('sights.list',$getParams)}}">Всі Категорії</a></li>
                     @foreach($sightList->categories() as $id=>$cat)
-                        <li><i class="fas fa-chevron-right caret"></i>
-                            @php($getParams = $sightList->filters(['category'=>$id],['subcategory']))
-                            <a href="{{route('sights.list',$getParams)}}">{{$cat['name']}}</a>
+                        @php($getParams = $sightList->filters(['category'=>$id],['subcategory']))
+                        @php($active=($id == $filter_category_id) ? 'active' : '')
+                        @php($bold = ($active) ? 'bold' : '')
+                        <li class="{{$active}}"><i class="fas fa-chevron-down caret"></i>
+                            <a class="{{$bold}}" category-id="{{$id}}" href="{{route('sights.list',$getParams)}}">{{$cat['name']}}</a>
                             <ul class="nested folding">
                             @foreach($cat['subcats'] as $s_id=>$s_name)
                                 @php($getParams = $sightList->filters(['subcategory'=>$s_id],['category']))
-                                <li><a class="link-secondary" href="{{route('sights.list',$getParams)}}">{{$s_name}}</a></li>
+                                @php($bold = ($filter_subcategory_id == $s_id) ? 'bold' : '')
+                                <li><a subcategory-id="{{$s_id}}" class="link-secondary {{$bold}}" href="{{route('sights.list',$getParams)}}">{{$s_name}}</a></li>
                             @endforeach
                             </ul>
                         </li>
