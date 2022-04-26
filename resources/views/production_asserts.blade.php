e98229e236a13eef81b145278c4c4bb0ce653fbd
tunik <tunik@detali.zp.ua>
2022-04-26 08:46:43 +0300

    анимация добавления локации в маршрут

--- resources/views/production_asserts.blade.php
+++ resources/views/production_asserts.blade.php
@@ -4,7 +4,7 @@
 @endphp
 
 @if($type=='css')
-    <link href="{{ asset('build/20220426083830.css') }}{{$gz}}" rel="stylesheet">
+    <link href="{{ asset('build/20220426084634.css') }}{{$gz}}" rel="stylesheet">
 @elseif($type=='js')
-    <script src="{{ asset('build/20220426083830.js') }}{{$gz}}"></script>
+    <script src="{{ asset('build/20220426084634.js') }}{{$gz}}"></script>
 @endif
