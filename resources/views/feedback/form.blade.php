f08be1e0b985c30fb28234590f136c942694c793
tunik <tunik@detali.zp.ua>
2022-07-12 15:06:19 +0300

    контент на форме обратной связи

--- resources/views/feedback/form.blade.php
+++ resources/views/feedback/form.blade.php
@@ -1,12 +1,21 @@
 @extends('layout')
 
+@section('head')
+	<title>{{env('APP_NAME')}}: Залишити вiдгук</title>
+@endsection
+
 @section('content')
 <div class="container">
+	<h1>Залишити вiдгук</h1>
 	<form action="{{route('feedback.create')}}" method="POST">
 		@csrf
 
 		<div class="form-group">
-			<label for="text">Залишити вiдгук:</label>
+			<label for="text">
+				Буль-ласка, напиши, що думаєш про цей проєкт. 
+				Якщо помітив помилку, чи може маєш ідею що тут можна покращити - ми будемо дуже раді почути твій відгук! 
+				Проєкт ще тільки розвивається, тому зворотній зв'язок від наших користувачів дуже важливий для нас
+			</label>
 			<textarea name="text" class="form-control" rows="5" autofocus></textarea>
 		</div>
 		<br />
