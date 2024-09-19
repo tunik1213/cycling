4177b7826ca443f6b9bc20222fc99feca58008e7
tunik1213 <fabler77@gmail.com>
2024-09-19 06:22:51 +0000

    fixed tinymce in route editor

--- resources/views/routes/edit.blade.php
+++ resources/views/routes/edit.blade.php
@@ -135,7 +135,6 @@
 @endsection
 
 @section('javascript')
-<script src="https://cdn.tiny.cloud/1/no-api-key/tinymce/5/tinymce.min.js" referrerpolicy="origin"></script>
 <script src='/draganddrop.js' type='text/javascript'></script>
 
 <script type="text/javascript">
